312f1e21eee3782afa2f8f47aab4e9dc53f26240
Kartik Sharma <kartiksharma4418@gmail.com>
2026-02-27 22:17:38 +0530

    halfway through

---
+++ public/js/chessgame.js
@@ -0,0 +1,27 @@
+const socket = io();
+const chess = new Chess();
+const boardElement = document.querySelector(".chessboard");
+
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderBoard = () => {
+    const board = chess.board();
+    boardElement.innerHTML = ""; 
+
+    board.forEach((row, rowIndex) => {
+     
+        row.forEach((square, squareIndex) => {
+            console.log(square); 
+            
+        });
+    });
+};
+    
+
+const handleMove = () => {};
+
+const getPieceUnicode = () => {};
+
+renderBoard();
